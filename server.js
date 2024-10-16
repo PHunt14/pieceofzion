f4c387f892217814ed326ce2e1a29ac5f96301c8
PHunt14 <PHunt14@users.noreply.github.com>
2024-10-15 21:12:00 -0400

    feat: make this a node app (#7)

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public-html')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
