5744affb783ba96abd7955403bea3740165dda22
PHunt14 <PHunt14@users.noreply.github.com>
2024-10-17 22:43:00 -0400

    fix: correct the web path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public-html')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public-html', 'index.html'));
 });
 
 app.listen(PORT, () => {
