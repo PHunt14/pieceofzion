d4f1c7cf756902048f171fadf46b9702d99a4578
PHunt14 <PHunt14@users.noreply.github.com>
2024-10-20 08:35:58 -0400

    feat: add tests

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Import AWS SDK v3 and configure it
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
-const client = new SecretsManagerClient({ region: 'us-east-1' }); // Replace with your AWS region
+const client = new SecretsManagerClient({ region: 'us-east-1' });
 
-// Function to get secret value
 async function getSecretValue(secretName) {
     try {
         const command = new GetSecretValueCommand({ SecretId: MAP_API_KEY });
@@ -21,11 +19,9 @@ async function getSecretValue(secretName) {
     return null;
 }
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public-html'));
 
-// Serve static files from the public-html folder
 app.use(express.static(path.join(__dirname, 'public-html')));
 
 app.get('/', (req, res) => {
@@ -33,10 +29,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/contact', async (req, res) => {
-    const awsApiKey = await getSecretValue('MAP_API_KEY'); // Replace with your secret name
+    const awsApiKey = await getSecretValue('MAP_API_KEY');
     res.render('contact', { awsApiKey: awsApiKey });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = { app, server };
