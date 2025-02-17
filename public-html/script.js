bff77d62d4e8bcafbe50406bbc0da7562304401d
PHunt14 <PHunt14@users.noreply.github.com>
2025-02-17 16:06:17 -0500

    feat: add the contact page to the index

--- public-html/script.js
+++ public-html/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // code for toggle sections
     const toggleButtons = document.querySelectorAll(".toggle-section");
 
     toggleButtons.forEach(button => {
@@ -9,4 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
             content.style.display = isVisible ? "none" : "block";
         });
     });
+
+    // New code for contact form
+    emailjs.init("_ds9K-Q5FnLJdIWlC");
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            message: event.target.message.value
+        };
+        sendEmail(formData);
+    });
+
+    function sendEmail(formData) {
+        emailjs.send("service_0j5giav", "template_3iw8vty", {
+            from_name: formData.name,
+            message: formData.message,
+            email: formData.email
+        })
+        .then(function(response) {
+            alert('Email sent successfully!');
+        }, function(error) {
+            alert('Failed to send email: ' + error.text);
+        });
+    }
 });
